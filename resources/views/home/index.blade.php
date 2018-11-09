25bb60cad76ca8cc2b2d5369c5e2d46b24a95b3b
SaiyanShivvy <hdiproductionsoffical@gmail.com>
2018-11-10 03:54:42 +1300

    Database Migrations Not Working.
    Signed-off-by: SaiyanShivvy <hdiproductionsoffical@gmail.com>

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,9 @@
+@extends('shared.layouts.master')
+@section('title',"Home")
+@section('content')
+    <div class="container">
+        <div class="row">
+            <p>Hello</p>
+        </div>
+    </div>
+@endsection
